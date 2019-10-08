556fa58fd4feb5f625b20ed9284cfb1ce0658237
jeffersoncarvalho <jeffersoncarvalho@gmail.com>
2019-10-08 19:52:58 -0300

    apagando

--- IMPLEMENTACOES/parte2/projeto_auth/src/App.js
+++ IMPLEMENTACOES/parte2/projeto_auth/src/App.js
@@ -8,16 +8,7 @@ import LoginForm from './components/LoginForm';
 export default class App extends Component{
 
   componentDidMount(){
-    /*firebase.initializeApp({
-      apiKey: 'AIzaSyCOWJXKYoVE74VLoC5NwDVFCtXTTDnV3Ik',
-      authDomain: "autenticar-600a4.firebaseapp.com",
-      databaseURL: "https://autenticar-600a4.firebaseio.com",
-      projectId: "autenticar-600a4",
-      storageBucket: "",
-      messagingSenderId: "719799626205",
-      appId: "1:719799626205:web:57281285dd3fa8bfb26e34",
-      measurementId: "G-EKV411S82V"
-    });*/
+    
   }
 
   render(){
